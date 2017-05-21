a20304a15006a4e6477da001dbcfdc17ffdadaf3
Sam Estep <sam@samestep.com>
2017-05-21 18:21:40 -0400

    Add Haskell

--- env.sh
+++ env.sh
@@ -52,6 +52,9 @@ boot -u
 wget -N https://storage.googleapis.com/golang/go1.8.1.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.8.1.linux-amd64.tar.gz
 
+sudo apt install haskell-stack
+stack setup
+
 cargo install racer
 rustup component add rust-src
 
