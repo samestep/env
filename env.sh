01037592b2e648b80c884316b7505b7c84ee33ec
Sam Estep <sam@samestep.com>
2017-05-21 15:54:59 -0400

    Update Racer when possible

--- env.sh
+++ env.sh
@@ -52,7 +52,7 @@ boot -u
 wget -N https://storage.googleapis.com/golang/go1.8.1.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.8.1.linux-amd64.tar.gz
 
-cargo install racer
+cargo install --force racer
 rustup component add rust-src
 
 sudo apt install nautilus-dropbox
