fc9b4f2e50648ed228f8f0cef40789ec42fdf12d
Sam Estep <sam@samestep.com>
2017-05-21 21:49:22 -0400

    Use Haskell Platform instead of Stack
    The Stack you can get from apt doesn't seem to be able to compile things in
    recent versions of Ubuntu, and the newer versions of Stack seem to have DNS
    problems, at least on my machine.

--- env.sh
+++ env.sh
@@ -52,8 +52,7 @@ boot -u
 wget -N https://storage.googleapis.com/golang/go1.8.1.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.8.1.linux-amd64.tar.gz
 
-sudo apt install haskell-stack
-stack setup
+sudo apt install haskell-platform
 
 cargo install racer
 rustup component add rust-src
