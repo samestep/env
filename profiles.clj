89bca6776e20827f0892d652315778d0bdb92bcc
Sam Estep <sam@samestep.com>
2017-06-02 20:16:54 -0400

    Add tools in Leiningen user profile

--- profiles.clj
+++ profiles.clj
@@ -1,2 +1,6 @@
 {:repl {:plugins [[cider/cider-nrepl "0.15.0-SNAPSHOT"]
-                  [refactor-nrepl "2.3.0-SNAPSHOT"]]}}
+                  [refactor-nrepl "2.3.0-SNAPSHOT"]]}
+ :user {:dependencies [[alembic "0.3.2"]
+                       [org.clojure/tools.namespace "0.2.11"]
+                       [proto-repl "0.3.1"]
+                       [proto-repl-charts "0.3.2"]]}}
