cb9cc1ed8501c323bc069514d7ffdc79e8372684
Sam Estep <sam@samestep.com>
2017-06-11 20:50:27 -0400

    Add language-rust to Atom

--- env.sh
+++ env.sh
@@ -24,6 +24,7 @@ apm install git-plus@7.8.0
 apm install go-plus@5.5.2
 apm install ink@0.6.5
 apm install keyboard-scroll@0.7.0
+apm install language-rust@0.4.10
 apm install latextools@0.8.5
 apm install proto-repl@1.4.20
 apm install proto-repl-charts@0.4.1
