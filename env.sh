4dbec98e340eb2e2b69b047d699dd129f82f9e31
Sam Estep <sam@samestep.com>
2017-12-20 14:24:56 -0500

    Add Atom wordcount package

--- env.sh
+++ env.sh
@@ -34,6 +34,7 @@ apm install platformio-ide-terminal@2.5.0
 apm install proto-repl@1.4.20
 apm install proto-repl-charts@0.4.1
 apm install set-syntax@0.3.2
+apm install wordcount@2.10.4
 ln -fs "$PWD/config.cson" ~/.atom/config.cson
 ln -fs "$PWD/init.coffee" ~/.atom/init.coffee
 ln -fs "$PWD/keymap.cson" ~/.atom/keymap.cson
