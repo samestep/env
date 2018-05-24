772bf87f3542926c35bbca24d066675eb54b0d9d
Sam Estep <sam@samestep.com>
2018-05-24 16:59:12 -0400

    Upgrade lilycompile
    See https://github.com/AljoschaMeyer/atom-lilycompile/issues/6

--- env.sh
+++ env.sh
@@ -30,7 +30,7 @@ apm install language-idris@0.4.9
 apm install language-matlab@0.2.1
 apm install language-rust@0.4.10
 apm install latextools@0.8.5
-apm install lilycompile@0.6.0
+apm install lilycompile@0.7.0
 apm install linter-eslint@8.4.1
 apm install lisp-paredit@0.5.5
 apm install parinfer@1.20.0
