01b1fd2cbfddb0f23a1772a80c1ddfe9e1496b81
Sam Estep <sam@samestep.com>
2019-08-01 11:43:40 -0400

    Add lein-exec

--- profiles.clj
+++ profiles.clj
@@ -3,4 +3,5 @@
                        [proto-repl-charts "0.3.2"]]
         :plugins [[jonase/eastwood "0.2.4"]
                   [lein-cljfmt "0.5.6"]
-                  [lein-cloverage "1.0.9"]]}}
+                  [lein-cloverage "1.0.9"]
+                  [lein-exec "0.3.7"]]}}
