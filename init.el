6a5bd52be2237012e3c49fab412b5ff224d63342
Sam Estep <sam@samestep.com>
2019-08-04 16:48:08 -0400

    Add boilerplate to Emacs script

--- init.el
+++ init.el
@@ -178,3 +178,17 @@
         font-latex-fontify-script nil))
 
 (use-package zenburn-theme)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (zenburn-theme use-package tagedit smex rainbow-delimiters racer projectile project-explorer magit impatient-mode ido-yes-or-no flycheck-rust flycheck-pos-tip evil-smartparens company clj-refactor cargo auctex))))
+(custom-set-faces)
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
