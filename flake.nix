08c9a6deee907df21dac80f8fe2411e8c18081af
Sam Estep <sam@samestep.com>
2026-05-05 08:32:34 -0400

    Update to fix GHSA-vh5x-56v6-4368

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-25.11-small";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
