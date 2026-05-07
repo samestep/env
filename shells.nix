ece3a50458a9eab43c5b0cbb55b014af501eef60
Sam Estep <sam@samestep.com>
2026-05-07 16:52:24 -0400

    Remove some unused dev shells

--- shells.nix
+++ shells.nix
@@ -1,12 +1,5 @@
 { pkgs }:
 {
-  definitely-typed = pkgs.mkShellNoCC {
-    buildInputs = [
-      pkgs.dprint
-      pkgs.nodejs
-      pkgs.pnpm
-    ];
-  };
   node = pkgs.mkShellNoCC {
     buildInputs = [
       pkgs.nodejs
@@ -27,15 +20,6 @@
     buildInputs = [
       pkgs.python3
       pkgs.uv
-    ];
-  };
-  vscode = pkgs.mkShellNoCC {
-    buildInputs = [
-      pkgs.clang
-      pkgs.krb5
-      pkgs.llvm
-      pkgs.nodejs_20
-      pkgs.python3
     ];
   };
   yarn = pkgs.mkShellNoCC {
