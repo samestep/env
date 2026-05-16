1dcbefe7172b7e829e49e15f3f8a74f3ff7769ec
Sam Estep <sam@samestep.com>
2026-05-16 16:15:01 -0400

    Revert "Use `libvirt_guest` specifically"
    This reverts commit 3a7e717545187eaa89ed9ca2f76c79ed93416529.

--- nixos/nixos/configuration.nix
+++ nixos/nixos/configuration.nix
@@ -135,10 +135,7 @@
   programs.virt-manager.enable = true;
 
   # https://libvirt.org/nss.html
-  virtualisation.libvirtd.nss = {
-    enable = true; # https://github.com/NixOS/nixpkgs/pull/499440
-    enableGuest = true; # The `libvirt_guest` variant works in more situations.
-  };
+  virtualisation.libvirtd.nss.enable = true;
   virtualisation.libvirtd.onShutdown = "shutdown"; # Else DHCP leases disappear.
 
   # Build virt-install with Ubuntu 26.04 support.
